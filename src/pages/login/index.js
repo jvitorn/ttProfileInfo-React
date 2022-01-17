b4dba186b02c25465adad5af443fa918e7b74302
jvitorn <joaoeuno2011@gmail.com>
2022-01-17 12:49:32 -0300

    add page home

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,36 +6,37 @@ import './style.css';
 export default function Logon() {
 
   return (
-    <Container>
-      <Row className="justify-content-center mt-5">
-        <Col md={12} lg={10} className="rounded">
-          <Row>
-            <Col md={12} className="p-md-5">
-              <div className="d-flex justify-content-center">
-                <div className='w-75'>
-                  <img className='img-fluid rounded-circle mx-auto d-block' src='https://i.pinimg.com/originals/71/7c/91/717c910defbeae6273038f2a1cfcdb2f.gif' />
+    <section className="mt-5">
+      <Container className='mb-3 mt-5 bg-white text-dark rounded p-5'>
+        <Row className="justify-content-center mt-5">
+          <Col md={12} lg={10} className="rounded">
+            <Row>
+              <Col md={12} className="p-md-5">
+                <div className="d-flex justify-content-center">
+                  <div className='w-75'>
+                    <img className='img-fluid rounded-circle mx-auto d-block' src='https://i.pinimg.com/originals/71/7c/91/717c910defbeae6273038f2a1cfcdb2f.gif' />
+                  </div>
                 </div>
-              </div>
-              <Form>
-                <Form.Group className="mb-3">
-                  <Form.Label>Usuario:</Form.Label>
-                  <Form.Control type="email" size="lg" placeholder="Digite o usúario" />
-                </Form.Group>
-                <Form.Group className="mb-5">
-                  <Form.Label>Senha:</Form.Label>
-                  <Form.Control type="password" size="lg" placeholder="Digite a senha" />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                  <button type="submit" className="form-control btn-orange rounded btn-lg">
-                    Entrar
-                  </button>
-                </Form.Group>
-              </Form>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Usuario:</Form.Label>
+                    <Form.Control type="email" size="lg" placeholder="Digite o usúario" />
+                  </Form.Group>
+                  <Form.Group className="mb-5">
+                    <Form.Label>Senha:</Form.Label>
+                    <Form.Control type="password" size="lg" placeholder="Digite a senha" />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <button type="submit" className="form-control btn-orange rounded btn-lg">
+                      Entrar
+                    </button>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter,Route,Routes as Rotas } from  'react-router-dom';
 //inport das paginas
 import Login from './pages/login';
+import Home  from './pages/home';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Rotas>
                 <Route path="/" element={<Login/>}/>
+                <Route path="/home" element={<Home/>}/>
             </Rotas>
         </BrowserRouter>
     );
